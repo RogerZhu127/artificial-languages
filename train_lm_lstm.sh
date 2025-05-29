bd993ddad7af7f042bfa5cf3cb4cf940c7639442
RogerZhu <y485zhu@uwaterloo.ca>
2025-05-29 17:31:36 -0400

    update experiment settings

--- train_lm_lstm.sh
+++ train_lm_lstm.sh
@@ -33,21 +33,23 @@ fairseq-train --task language_modeling "data-bin/${GRAMMAR}/${SPLIT}-dataset" \
     --weight-decay 0.01 \
     --lr 0.0005 \
     --lr-scheduler inverse_sqrt \
-    --warmup-updates 4000 \
+    --warmup-updates 400 \
     --clip-norm 0.0 \
     --warmup-init-lr 1e-07 \
-    --tokens-per-sample 512 \
+    --tokens-per-sample 128 \
     --sample-break-mode none \
-    --max-tokens 4096 \
-    --update-freq 16 \
+    --max-tokens 512 \
+    --update-freq 4 \
     --patience 5 \
     --max-update 10000 \
     --no-epoch-checkpoints \
     --no-last-checkpoints \
-    --max-epoch 10
+    --max-epoch 10 \
+    --fp16 \
+    --reset-optimizer
 
-fairseq-eval-lm "data-bin/${GRAMMAR}/${SPLIT}-dataset" --path "checkpoints/${GRAMMAR}/${SPLIT}-lstm/checkpoint_best.pt" --tokens-per-sample 512 --gen-subset "valid" --output-word-probs --quiet 2> "lstm-results/${GRAMMAR}.${SPLIT}.dev.txt"
+fairseq-eval-lm "data-bin/${GRAMMAR}/${SPLIT}-dataset" --path "checkpoints/${GRAMMAR}/${SPLIT}-lstm/checkpoint_best.pt" --tokens-per-sample 128 --gen-subset "valid" --output-word-probs --quiet 2> "lstm-results/${GRAMMAR}.${SPLIT}.dev.txt"
 
-fairseq-eval-lm "data-bin/${GRAMMAR}/${SPLIT}-dataset" --path "checkpoints/${GRAMMAR}/${SPLIT}-lstm/checkpoint_best.pt" --tokens-per-sample 512 --gen-subset "test" --output-word-probs --quiet 2> "lstm-results/${GRAMMAR}.${SPLIT}.test.txt"
+fairseq-eval-lm "data-bin/${GRAMMAR}/${SPLIT}-dataset" --path "checkpoints/${GRAMMAR}/${SPLIT}-lstm/checkpoint_best.pt" --tokens-per-sample 128 --gen-subset "test" --output-word-probs --quiet 2> "lstm-results/${GRAMMAR}.${SPLIT}.test.txt"
 
 python get_sentence_scores.py -i "lstm-results/${GRAMMAR}.${SPLIT}.test.txt" -O "sentence_scores_lstm/"
