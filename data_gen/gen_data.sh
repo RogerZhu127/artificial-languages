d1c875300dc2ae664c048abc2baf7367933e18b9
RogerZhu <y485zhu@uwaterloo.ca>
2025-05-29 14:22:56 -0400

    update settings

--- data_gen/gen_data.sh
+++ data_gen/gen_data.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 python sample_sentences.py -g base-grammar.gr -n 100000 -O . -b True
 python permute_sentences.py -s sample_base-grammar.txt -O permuted_samples/ 
-python make_splits.py -S permuted_samples/ -O permuted_splits/ -n 5
+python make_splits.py -S permuted_samples/ -O permuted_splits/ -n 5 -tr 0.8 -ts 0.1 -dv 0.1
