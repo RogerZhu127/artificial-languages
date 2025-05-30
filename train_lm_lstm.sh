b7fdde6b69bbefeffd203c32141bdc0f933fa6f6
ruoxining <ruoxining@outlook.com>
2025-05-29 20:14:18 -0400

    [chore] update settings

--- train_lm_lstm.sh
+++ train_lm_lstm.sh
@@ -33,18 +33,20 @@ fairseq-train --task language_modeling "data-bin/${GRAMMAR}/${SPLIT}-dataset" \
     --weight-decay 0.01 \
     --lr 0.0005 \
     --lr-scheduler inverse_sqrt \
-    --warmup-updates 4000 \
+    --warmup-updates 400 \
     --clip-norm 0.0 \
     --warmup-init-lr 1e-07 \
-    --tokens-per-sample 512 \
+    --tokens-per-sample 128 \
     --sample-break-mode none \
-    --max-tokens 4096 \
-    --update-freq 16 \
+    --max-tokens 512 \
+    --update-freq 4 \
+    --decoder-layers 2 \
     --patience 5 \
     --max-update 10000 \
     --no-epoch-checkpoints \
     --no-last-checkpoints \
-    --max-epoch 10
+    --max-epoch 10 \
+    --reset-optimizer
 
 fairseq-eval-lm "data-bin/${GRAMMAR}/${SPLIT}-dataset" --path "checkpoints/${GRAMMAR}/${SPLIT}-lstm/checkpoint_best.pt" --tokens-per-sample 512 --gen-subset "valid" --output-word-probs --quiet 2> "lstm-results/${GRAMMAR}.${SPLIT}.dev.txt"
 
--- train_lm_transformer.sh
+++ train_lm_transformer.sh
@@ -26,26 +26,32 @@ fairseq-train --task language_modeling "data-bin/${GRAMMAR}/${SPLIT}-dataset" \
     --decoder-layers 2 \
     --decoder-attention-heads 2 \
     --decoder-embed-dim 128 \
+    --activation-fn gelu \
     --decoder-ffn-embed-dim 512 \
+    --decoder-input-dim 128 \
+    --decoder-output-dim 128 \
+    --decoder-normalize-before \
     --share-decoder-input-output-embed \
+    --attention-dropout 0.1 \
     --dropout 0.3 \
     --optimizer adam \
     --adam-betas '(0.9,0.98)' \
     --weight-decay 0.01 \
     --lr 0.0005 \
     --lr-scheduler inverse_sqrt \
-    --warmup-updates 4000 \
+    --warmup-updates 400 \
     --clip-norm 0.0 \
     --warmup-init-lr 1e-07 \
-    --tokens-per-sample 512 \
+    --tokens-per-sample 128 \
     --sample-break-mode none \
-    --max-tokens 4096 \
-    --update-freq 16 \
-    --patience 5 \
+    --max-tokens 512 \
+    --update-freq 4 \
+    --patience -1 \
     --max-update 10000 \
     --no-epoch-checkpoints \
     --no-last-checkpoints \
-    --max-epoch 10
+    --max-epoch 10 \
+    --reset-optimizer
 
 fairseq-eval-lm "data-bin/${GRAMMAR}/${SPLIT}-dataset" --path "checkpoints/${GRAMMAR}/${SPLIT}-transformer/checkpoint_best.pt" --tokens-per-sample 512 --gen-subset "valid" --output-word-probs --quiet 2> "trans-results/${GRAMMAR}.${SPLIT}.dev.txt"
 
